7433e7d3e8052750c7ad6fdbb062ebee2fb03ec7
pinguwu <premixedcloud977@gmail.com>
2018-12-19 12:42:29 -0800

    m

--- webapp.py
+++ webapp.py
@@ -1,51 +1,46 @@
-from flask import Flask, url_for, render_template, request, Markup
+
+from flask import Flask, url_for, render_template, request, flash, Markup
 import os
 import json
 app = Flask(__name__)
 
-@app.route("/") #the default thing
+def fun_fact(state):
+    with open('state_demographics.json') as demographics_data:
+        states = json.load(demographics_data)
+    boi = 0
+    for s in states:
+        if s["State"] == state:
+            boi = s["Income"]["Median Houseold Income"]
+    
+    print (state + "'s Median Income is " + str(boi))
+    return state + "'s Median Income is " + str(boi)
+
+@app.route("/") #annotation tells the URL that will make this function run
 def render_main():
     with open('state_demographics.json') as demographics_data:
-        statess = json.load(demographics_data)
+        states = json.load(demographics_data)
     try:
         state = request.args['states']
-        madre = get_fun_fact(state)
-        return render_template('index.html', states = get_state_options(states), funfact = madre)
+        ff = fun_fact(state)
+        return render_template("index.html", states = get_state_options(states), funfact = ff(state))
     except:
-        return render_template('index.html', states = get_state_options(states))
+        return render_template("index.html", states = get_state_options(states))
 
 def get_state_options(states):
-  yeetus=[]
+  bom=[]
 
   for state in states:
     state = state["State"]
-    chief = state in yeetus
-    if (chief == False):
-      yeetus.append(state)
+    trfl = state in bom
+    if (trfl == False):
+      bom.append(state)
 
 
   options = ""
-  for state in yeetus:
+  for state in bom:
     options += Markup("<option value=\"" + state + "\">" + state + "</option>")
 
   return options
 
-def get_fun_fact(staterino):
-    state = request.args["states"]
-
-    with open('state_demographics.json') as list_o_things:
-        info = json.load(list_o_things)
-
-        #highest median income i guess lmao
-
-    bigboi = 0
-    for state in info:
-        if state["State"] == staterino:
-            if (state["Income"]["Median Household Income"] > bigboi):
-                bigboi = state["Income"]["Median Household Income"]
-                higheststate = state["State"]
-
-    return state + "'s Median Household Income is " +  "$" + "{:,}".format(bigboi)
-
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=66666)
