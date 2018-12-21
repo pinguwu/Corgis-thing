5afefab9dfe474f6ffec42a1fa0e59333c9aafb0
pinguwu <premixedcloud977@gmail.com>
2018-12-21 10:36:34 -0800

    k

--- webapp.py
+++ webapp.py
@@ -4,6 +4,10 @@ import os
 import json
 app = Flask(__name__)
 
+@app.route("/")
+def render_main():
+    return render_template("home.html")
+
 def fun_fact(state):
     with open('state_demographics.json') as demographics_data:
         states = json.load(demographics_data)
@@ -13,18 +17,18 @@ def fun_fact(state):
             boi = s["Income"]["Median Houseold Income"]
     
     print (state + "'s Median Income is " + str(boi))
-    return state + "'s Median Income is " + str(boi)
+    return state + "'s Median Income is $" + str(boi)
 
-@app.route("/") #annotation tells the URL that will make this function run
-def render_main():
+@app.route("/med") #annotation tells the URL that will make this function run
+def render_med():
     with open('state_demographics.json') as demographics_data:
         states = json.load(demographics_data)
     try:
         state = request.args['states']
         ff = fun_fact(state)
-        return render_template("index.html", states = get_state_options(states), funfact = ff(state))
+        return render_template("med.html", states = get_state_options(states), funfact = ff)
     except:
-        return render_template("index.html", states = get_state_options(states))
+        return render_template("med.html", states = get_state_options(states))
 
 def get_state_options(states):
   bom=[]
@@ -42,5 +46,10 @@ def get_state_options(states):
 
   return options
 
+
+@app.route("/about")
+def render_about():
+    return render_template("about.html")
+
 if __name__=="__main__":
-    app.run(debug=True, port=66666)
+    app.run(debug=False, port=66666)
